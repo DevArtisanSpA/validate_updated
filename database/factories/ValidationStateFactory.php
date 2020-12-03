6cb2329f402101324eac8b11c8652837b4e051e3
Lucas <lucas@devartisan.cl>
2020-12-03 16:12:44 -0300

    adding plain controllers, models and db files

---
+++ database/factories/ValidationStateFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ValidationState;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ValidationStateFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ValidationState::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
